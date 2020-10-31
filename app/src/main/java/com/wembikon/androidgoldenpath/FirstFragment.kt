63f521d47ef5db4bc5da5d14583ff5a44f18d07c
adrian <wembikon@outlook.com>
2020-10-31 11:21:26 +0100

    Reformat app src folder using editorconfig

--- app/src/main/java/com/wembikon/androidgoldenpath/FirstFragment.kt
+++ app/src/main/java/com/wembikon/androidgoldenpath/FirstFragment.kt
@@ -13,19 +13,19 @@ import androidx.navigation.fragment.findNavController
  */
 class FirstFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_first, container, false)
+  }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+    view.findViewById<Button>(R.id.button_first).setOnClickListener {
+      findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
     }
+  }
 }
--- app/src/main/java/com/wembikon/androidgoldenpath/SecondFragment.kt
+++ app/src/main/java/com/wembikon/androidgoldenpath/SecondFragment.kt
@@ -13,19 +13,19 @@ import androidx.navigation.fragment.findNavController
  */
 class SecondFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
-    }
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_second, container, false)
+  }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+    view.findViewById<Button>(R.id.button_second).setOnClickListener {
+      findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
     }
+  }
 }
